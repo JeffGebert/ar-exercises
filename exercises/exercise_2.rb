87ad7f70e387c4c1e91afa729fd9c8708cfbc8f3
Jeff Gebert <jeffgebert@hotmail.com>
2019-12-09 19:03:05 +0000

    finished exercise 1 & 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,13 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.name = "Regina"
+
+puts @store1.name
+puts @store2
+
+### Exercise 2: Update the first store
+
+
